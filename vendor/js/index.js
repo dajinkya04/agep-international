85cc065eea8fca58953b4790d62b41c181cff47e
Ajinkya Deshmukh <111303410+dajinkya04@users.noreply.github.com>
2023-07-18 11:32:20 +0530

    all data uploded

---
+++ vendor/js/index.js
@@ -0,0 +1,18 @@
+
+  /**
+   * Initiate portfolio lightbox 
+   */
+  var portfolioLightbox =GLightbox({
+    selector: '.portfolio-lightbox'
+  });
+
+  /**
+   * Initiate glightbox 
+   */
+  var gLightbox = GLightbox({
+    selector: '.glightbox'
+  });
+
+  
+
+  
